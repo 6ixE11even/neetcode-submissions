8fb569ad5d2198bfacce7f5c5d76ef3633a2e2c8
Tejas Pandya <tejaspandya67@gmail.com>
2026-07-10 11:28:12 -0400

    Add: anagram-groups - submission-3

---
+++ Data Structures & Algorithms/anagram-groups/submission-3.py
@@ -0,0 +1,32 @@
+class Solution:
+    def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
+        output = []
+        if len(strs) == 0:
+            output.append([""])
+            return output
+
+        if len(strs) == 1:
+            output.append(strs)
+            return output
+
+        # Sorting Method
+        # each anagram after sorting returns same signature
+        # eg. "act" and "cat" both becomes "act" after sorting
+        # eg. "stop", "pots", "tops" all become "opst" after sorting
+        # we will check if the signature exists in a hashmap, if not add it as a key and word as value
+        # we will return list of dict.values as final output
+
+        hashmap = {}
+        key_list = []
+
+
+        for word in strs:
+            signature = tuple(sorted(word))
+            if signature not in hashmap:
+                key_list = []
+                key_list.append(word)
+                hashmap[signature] = key_list
+            else:
+                hashmap.get(signature).append(word)
+
+        return list(hashmap.values())
